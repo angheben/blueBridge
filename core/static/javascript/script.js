b5d5dd32eaa0a4c262c7647b83667e8d0ff4663a
angheben <vitor_angheben@hotmail.com>
2024-04-04 10:05:25 -0300

    Fixing Front End

--- core/static/javascript/script.js
+++ core/static/javascript/script.js
@@ -1,35 +1,17 @@
 /*
-window.addEventListener('scroll', function() {
-    const aboutSection = document.getElementById('about');
-    const position = aboutSection.getBoundingClientRect();
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        const aboutSection = document.getElementById('about');
+        const position = aboutSection.getBoundingClientRect();
 
-    // Show the "about" section when it reaches a certain point on the page
-    if (position.top < window.innerHeight * 0.75) {
-        aboutSection.style.display = 'block';
-    } else {
-        aboutSection.style.display = 'none'; // Hide the section if it's not within the visible range
-    }
-});
-*/
+        console.log('Position top:', position.top, 'Window innerHeight:', window.innerHeight);
 
-// script.js
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        // Show the "about" section when it reaches a certain point on the page
+        if (position.top < window.innerHeight * 0.75) {
+            aboutSection.style.display = 'block';
+        } else {
+            aboutSection.style.display = 'none'; // Hide the section if it's not within the visible range
+        }
     });
 });
+*/
