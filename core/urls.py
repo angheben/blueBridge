67525aeb369a43e6e5f02b4f7e03362a91876d4b
angheben <vitor_angheben@hotmail.com>
2024-04-05 16:21:52 -0300

    Login and Register done

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,8 @@
-from .views import login,  signup # about, negotiate, index,
-from .views import ControlView
+from .views import ControlView, SignupView, LoginView
 from django.urls import path
-
 
 urlpatterns = [
     path("", ControlView.as_view(), name="control"),
-    path("signup/", signup, name="signup"),
-    path("login/", login, name="login"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
-
-"""
-    path("", index, name="index"),
-
-    path("negotiate/", negotiate, name='negotiate'),
-    path("about/", about, name='negotiate'),
-"""
--- core/views.py
+++ core/views.py
@@ -1,29 +1,53 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import SignupForm, LoginForm
+from django.urls import reverse_lazy
+from django.views.generic import RedirectView, FormView
+from django.contrib.auth import logout, authenticate, login
 
 
 class ControlView(TemplateView):
     template_name = "control.html"
 
 
-def login(request):
-    return render(request, template_name='login.html')
+class LoginView(FormView):
+    """
+    This class will serve as the login view
+    """
+    template_name = "login.html"
+    form_class = LoginForm
+    success_url = reverse_lazy("core:control")
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
-def signup(request):
-    return render(request, template_name='signup.html')
+class SignupView(FormView):
+    """
+    This class will serve to check some dependencies in the project accordingly to the forms provided
+    """
+    template_name = 'signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('core:login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return render(self.request, self.template_name, {'form': form})
 
 
+class LogoutView(RedirectView):
+    url = reverse_lazy('core:control')
 
-"""
-def index(request):
-    return render(request, template_name='index.html')
-
-
-def negotiate(request):
-    return render(request, template_name='negotiate.html')
-
-
-def about(request):
-    return render(request, template_name='about.html')
-"""
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super().get(request, *args, **kwargs)
